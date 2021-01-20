e7b43c090edf0e646312470ab288771066de8d1e
unknown <lele.robert18@gmail.com>
2021-01-20 18:57:59 -0300

    inserindo o codigo

---
+++ script.js
@@ -0,0 +1,102 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
+let isJumping = false;
+let isGameOver = false;
+let position = 0;
+
+function handleKeyUp(event) {
+  if (event.keyCode === 32) {
+    if (!isJumping) {
+      jump();
+    }
+  }
+}
+
+function jump() {
+  isJumping = true;
+
+  let upInterval = setInterval(() => {
+    if (position >= 200) {
+      // Descendo
+      clearInterval(upInterval);
+
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval);
+          isJumping = false;
+        } else {
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+      }, 20);
+    } else {
+      // Subindo
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
+}
+
+function createCactus() {
+  const cactus = document.createElement('div');
+  let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
+  if (isGameOver) return;
+
+  cactus.classList.add('cactus');
+  background.appendChild(cactus);
+  cactus.style.left = cactusPosition + 'px';
+
+  let leftTimer = setInterval(() => {
+    if (cactusPosition < -60) {
+      // Saiu da tela
+      clearInterval(leftTimer);
+      background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      // Game over
+      clearInterval(leftTimer);
+      isGameOver = true;
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + 'px';
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime);
+}
+
+function createpassaro() {
+  const passaro = document.createElement('div');
+  let passaroPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
+  if (isGameOver) return;
+
+  passaro.classList.add('passaro');
+  background.appendChild(passaro);
+  passaro.style.left = passaroPosition + 'px';
+
+  let leftTimer = setInterval(() => {
+    if (passaroPosition < -30) {
+      // Saiu da tela
+      clearInterval(leftTimer);
+      background.removeChild(passaro);
+    } else if (passaroPosition > 0 && passaroPosition < 30 && position < 30) {
+      // Game over
+      clearInterval(leftTimer);
+      isGameOver = true;
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+    } else {
+      passaroPosition -= 10;
+      passaro.style.left = passaroPosition + 'px';
+    }
+  }, 30);
+
+  setTimeout(createpassaro, randomTime);
+}
+createpassaro();
+createCactus();
+document.addEventListener('keyup', handleKeyUp);
